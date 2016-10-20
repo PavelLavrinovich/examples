5e3f9181503bb7748eb9ae6a2dd9085dc86346a2
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2016-10-20 12:29:22 +0300

    first commit

---
+++ archive_controller.rb
@@ -0,0 +1,30 @@
+class ArchiveController < ApplicationController
+  unloadable
+  accept_api_auth :index, :show
+
+  def index
+    query = Project.visible.map { |project| "project_id = #{project.id}" }.join(" or ")
+
+    if query != ''
+  	  @archive = params[:last_change_id] ? Archive.where("id > ? and (#{query})", params[:last_change_id]) : Archive.where(query)
+  	end
+    @archive ||= []
+
+  	respond_to do |format|
+      format.html 
+      format.xml { render xml: @archive }
+      format.json { render json: { archives: @archive.as_json(root: false) } } 
+    end     	
+  end
+
+  def show
+    @archive_line = Archive.find(params[:archive_id])
+    raise ::Unauthorized unless Project.find( @archive_line.project_id ).visible?
+
+    respond_to do |format|
+      format.html 
+      format.xml { render xml: @archive_line }
+      format.json { render json: @archive_line.as_json(root: true) } 
+    end
+  end
+end
---
+++ custom_fields_controller_patch.rb
@@ -0,0 +1,19 @@
+require 'custom_fields_controller'
+
+module Exonit
+    module Redmine
+        module Patches
+            module CustomFieldsControllerPatch
+                def self.included(base)
+                    base.class_eval do
+                        skip_before_filter :require_admin, :only => :index
+                    end
+                end
+            end
+        end
+    end
+end
+
+unless CustomFieldsController.included_modules.include? Exonit::Redmine::Patches::CustomFieldsControllerPatch
+    CustomFieldsController.send(:include, Exonit::Redmine::Patches::CustomFieldsControllerPatch)
+end
